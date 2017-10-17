9c0c6340d7dc5a7bb478eec64c1062394134a73e
Kevin Robinson <kevin.robinson.0@gmail.com>
2017-10-17 00:44:03 -0400

    Fix test

--- client/src/ThanksPhase.test.js
+++ client/src/ThanksPhase.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ThanksPhase from './ThanksPhase.js';
+import {logs} from './util/fixtures.js';
 
 
 it('renders without crashing', async () => {
   const div = document.createElement('div');
-  await ReactDOM.render(<ThanksPhase onInteraction={jest.fn()}/>, div);
+  await ReactDOM.render(<ThanksPhase
+    email="foo@mit.edu"
+    logs={logs}
+    onInteraction={jest.fn()}/>, div);
 });
