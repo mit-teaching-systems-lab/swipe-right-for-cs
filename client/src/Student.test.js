7c4c18500548d54a9a8d6090cd8fe71b54dc4513
Kevin Robinson <kevin.robinson.0@gmail.com>
2017-10-18 18:08:58 -0400

    Refactor Student and remove Turn to solve image blip:

--- client/src/Student.test.js
+++ client/src/Student.test.js
@@ -2,16 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Student from './Student.js';
 
+
+function testProps(props = {}) {
+  return {
+    ...props,
+    profileName: "Kevin",
+    profileKey: "WM1",
+    profileImageSrc: "WM1.png",
+    profileText: "Kevin likes trees.",
+    argumentTexts: ["Because it's fun", "Because it's cool"],
+    onDone: jest.fn(),
+    onInteraction: jest.fn(),
+  };
+}
+
 it('renders without crashing', async () => {
   const div = document.createElement('div');
-  ReactDOM.render(
-    <Student
-      profileName="Kevin"
-      profileKey="WM1"
-      profileImageSrc="WM1.png"
-      profileText="Kevin likes trees."
-      argumentTexts={["Because it's fun", "Because it's cool"]}
-      onDone={jest.fn()}
-      onInteraction={jest.fn()}
-    />, div);
+  const props = testProps();
+  ReactDOM.render(<Student {...props} />, div);
+});
+
+it('records swipe left interactions correctly', async () => {
+  const div = document.createElement('div');
+  const props = testProps();
+  const instance = ReactDOM.render(<Student {...props} />, div); // eslint-disable-line react/no-render-return-value
+  const turn = instance.currentSwipeTurn();
+  instance.onSwipeLeft(turn);
+  const expectedInteration = {
+    turn,
+    type: "SWIPE_LEFT:a3n0/aAfASRmbKZTpULy8BE9F8fCjsWoczycNGdwf5M="
+  };
+  expect(props.onInteraction).toHaveBeenCalledWith(expectedInteration);
+});
+
+it('records swipe right interactions correctly', async () => {
+  const div = document.createElement('div');
+  const props = testProps();
+  const instance = ReactDOM.render(<Student {...props} />, div); // eslint-disable-line react/no-render-return-value
+  const turn = instance.currentSwipeTurn();
+  instance.onSwipeRight(turn);
+  const expectedInteration = {
+    turn,
+    type: "SWIPE_RIGHT:JTfoWc+SuahkjxwGte2EYDAKTRv0Tjz2ktX/5vdZm00="
+  };
+  expect(props.onInteraction).toHaveBeenCalledWith(expectedInteration);
 });
