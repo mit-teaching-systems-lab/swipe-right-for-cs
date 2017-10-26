283ab7f9b97bd3cd2d0e250d6a1490bad0a1538f
Kevin Robinson <kevin.robinson.0@gmail.com>
2017-10-26 15:27:14 -0400

    Add linting

--- scripts/clone.js
+++ scripts/clone.js
@@ -1,18 +1,18 @@
-const pg = require('pg');
-const {Client} = require('pg')
+const {Client} = require('pg');
 const {createPool} = require('../server/database.js');
 
 // This script can be used to grab a snapshot of the database and
 // print it in a JSON format that can be used for offline analysis.
 //
 // Example usage:
-//   heroku run NODE_ENV=production node scripts/clone.js
+//   DATABASE_URL=foo NODE_ENV=development node scripts/clone.js
+//   or you can run in through Heroku and dump to local disk
 
 // Output json in the format {[tableName]: [row]}
 function clone(tableNames) {
   const postgresUrl = (process.env.NODE_ENV === 'development')
     ? process.env.DATABASE_URL
-    : process.env.DATABASE_URL +'?ssl=true'
+    : process.env.DATABASE_URL +'?ssl=true';
   
   const {escapeIdentifier} = new Client();
   const pool = createPool(postgresUrl);
@@ -29,7 +29,7 @@ function clone(tableNames) {
     .catch(err => {
       console.log('---- error ----');
       console.log(err);
-      process.exit(1);
+      process.exit(1); // eslint-disable-line no-process-exit
     })
     .then(results => {
       const output = results.reduce((map, result) => {
@@ -38,7 +38,7 @@ function clone(tableNames) {
         return map;
       }, {});
       console.log(JSON.stringify(output));
-      process.exit(0);
+      process.exit(0);  // eslint-disable-line no-process-exit
     });
 }
 
