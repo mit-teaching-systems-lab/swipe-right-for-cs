48512f53b883f6af29a9999e5c24d771dd0d1c76
gballard1 <gballard@mit.edu>
2017-11-03 15:43:43 -0400

    started making chartsgit add -A My next step is to convert data into the right format for the chart, figure out why there are only 2 names-carlos and meredith- and why carlos's name keeps repeating

--- client/src/research/InteractionsView.js
+++ client/src/research/InteractionsView.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {InteractionTypes} from '../shared/data.js';
-
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryGroup, VictoryAxis} from 'victory'; 
 
 // Render a list of logged user interactions
 class InteractionsView extends Component {
-  render() {  
-    // unpack!
-    //filter out testing data
+  onlySwipes(){
     const interactions = this.props.interactions.filter(row =>{  
       if (row.session.workshopCode === 'foo') return false;
       if (row.session.workshopCode === 'demo') return false;
@@ -18,12 +16,25 @@ class InteractionsView extends Component {
       if (row.session.identifier === 'kevin') return false;
       return true;
     });
+    return interactions.filter(row=>{
+      if (row.interaction.type === InteractionTypes.SWIPE_RIGHT || row.interaction.type === InteractionTypes.SWIPE_LEFT){
+        return true;
+      }
+      return false;
+    });
+  }
+  render() {  
+    // unpack!
+    //filter out testing data
+    const interactions = this.onlySwipes();
     // show it!
     //render data as a filterable table
     return( 
       <div>
         {this.renderPercentSwipeRight(interactions)}
-        {this.renderPercentRightPerProfile(interactions)}
+        {this.renderPercentRightPerProfile(interactions, 'profileKey')}
+        {this.renderPercentRightPerProfile(interactions, 'profileName')}
+        {this.renderChart(interactions, 'profileName')}
         <table>{interactions.map(row =>{
           return <tr key = {row.id}>
             <td> {row.id} </td>
@@ -45,27 +56,62 @@ class InteractionsView extends Component {
     return sum/interactions.length*100;
   }
 
-  renderPercentRightPerProfile(interactions){
+  renderPercentRightPerProfile(interactions, key){
     var totals = {};
     var percents = {};
     interactions.forEach(row =>{
       if (row.interaction.type === InteractionTypes.SWIPE_RIGHT || row.interaction.type === InteractionTypes.SWIPE_LEFT){
-        if (row.interaction.turn.profileKey in totals){
-          if (row.interaction.type === InteractionTypes.SWIPE_RIGHT) totals[row.interaction.turn.profileKey][0] += 1;
-          totals[row.interaction.turn.profileKey][1] += 1;
+        if (!(row.interaction.turn[key] in totals)){
+          totals[row.interaction.turn[key]] = [0,0];
         }
-        else{
-          totals[row.interaction.turn.profileKey] = [1,1];
+        if (row.interaction.type === InteractionTypes.SWIPE_RIGHT){ 
+          totals[row.interaction.turn[key]][0] += 1;
         }
-      }
-      for (var k in totals){
-        percents[k] = totals[k][0]/totals[k][1]*100 ;
+        totals[row.interaction.turn[key]][1] += 1;
       }
     });
-    return JSON.stringify(percents);
+    for(var k in totals){
+      percents[k] = totals[k][0]/totals[k][1]*100;
+    }
+    return <pre>{JSON.stringify(percents, null,2)}</pre>;
   }
+  renderChart(interactions, key){
+    var keys=[]; 
+    var d =[];
+    interactions.forEach(row =>{
+      if (!(row.interaction.turn[key] in keys)) keys.push(row.interaction.turn[key]);
+    });
 
-
+    return (
+      <div>
+        <VictoryChart
+          theme={VictoryTheme.material}
+          domain={{y: [0, 5.5]}}
+          style={{ parent: { maxWidth: "50%" } }}
+        >
+          <VictoryAxis dependentAxis={true} tickValues={keys}/>
+          <VictoryGroup horizontal
+            offset={1}
+            style={{ data: { width: 3 } }}
+            colorScale={["blue", "tomato", "gold"]}
+          >
+            <VictoryBar
+              data= {d}
+            />
+            <VictoryBar
+              data={[
+                {x: 1, y: 2},
+                {x: 2, y: 3},
+                {x: 3, y: 4},
+                {x: 4, y: 5},
+                {x: 5, y: 5}
+              ]}
+            />
+          </VictoryGroup>
+        </VictoryChart>
+      </div>
+    );
+  }
 }
 InteractionsView.propTypes = {
   interactions: PropTypes.array.isRequired
--- client/src/research/InteractionsView.story.js
+++ client/src/research/InteractionsView.story.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import {withFrameSwitcher} from '../util/storybookFrames.js';
 import InteractionsView from './InteractionsView.js';
 
 
@@ -8,7 +7,5 @@ storiesOf('InteractionsView', module) //eslint-disable-line no-undef
   .add('normal', () => {
     const db = require('../../../tmp/swipe-right-db.json');
     const {interactions} = db;
-    return withFrameSwitcher(
-      <InteractionsView interactions={interactions} />
-    );
+    return <InteractionsView interactions={interactions} />;
   });
