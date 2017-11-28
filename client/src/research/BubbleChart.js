4408d3f54afbda38f5316a3010a2f894f32e078f
gballard1 <gballard@mit.edu>
2017-11-28 17:42:52 -0500

    created table. rows: students' names; columns, percent swipe right per profile

--- client/src/research/BubbleChart.js
+++ client/src/research/BubbleChart.js
@@ -11,23 +11,52 @@ class BubbleChart extends Component{
     });//lodash group by {Name1:{maker:percent},Name3:{captain:percent}} {maker:{Name1: percent, Name2:percent}}
     return groups;
   }
+
   render(){
     const interactions = this.props.swipeInteractions;
-    var bubbleTable = document.getElementById("Bable: Bubble Table");
+    const profileKeys = _.uniq(_.map(interactions, row=>{
+      return row.interaction.turn.profileKey;
+    }));
     const groupedByName = this.grouping(interactions, 'profileName');
     const groupedByKey = _.mapValues(groupedByName, row => {
       return percentRightPerProfile(row, 'profileKey');
     }); 
-    _.forIn(groupedByKey, row=>{
-      var currentRow = document.createElement("TR");
-      _.forIn(row, col =>{
-        console.log(row[col])
-        currentRow.insertCell(row[col]);
-      });
-      console.log(currentRow)
-      bubbleTable.insertRow(currentRow);
-    });
-    return bubbleTable;
+
+    return (
+      <table>
+
+        <tr>
+          <td>Name</td>
+          {_.map(profileKeys, profileKey=>{
+            return <td>{profileKey}</td>;
+          })}
+        </tr>
+        {_.map(groupedByKey, (row, profileName) => {
+          return (
+            <tr>
+              <td>{profileName}</td>
+              {_.map(profileKeys, profileKey=>{
+                return <td>{row[profileKey]}</td>; 
+              })}
+            </tr>
+          );
+        })}
+      </table>
+    );
+
+
+  //   _.forIn(groupedByKey, row=>{
+  //     var currentRow = document.createElement("TR");
+  //     _.forIn(row, col =>{
+  //       console.log(row, col)
+  //       var cell = currentRow.insertCell();
+  //       cell.innerHTML = col
+  //     });
+  //     console.log(currentRow)
+  //     var r = bubbleTable.insertRow();
+  //     r.innerHTML = currentRow
+  //   });
+  //   return bubbleTable;
   } 
 }
 
