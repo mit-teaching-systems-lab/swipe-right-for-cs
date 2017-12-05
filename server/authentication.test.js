62b28d19e642c1c4f7ae9eba93dbf33aad927609
Kevin Ng <keving17@mit.edu>
2017-12-05 14:42:50 -0500

    fixes jest tests

--- server/authentication.test.js
+++ server/authentication.test.js
@@ -23,4 +23,24 @@ describe('loginEndpoint', () => {
       done();
     });
   });
+
+  it('returns 200 when user is in whitelist', done => {
+    const request  = httpMocks.createRequest({
+      method: 'POST',
+      url: '/login',
+      body: {
+        email : 'kevin@mit.edu'
+      }
+    });
+    console.log('kevin@mit.edu');
+    const response = testResponse();
+    const pool = testPool();
+    const mailgunEnv = {};
+
+    loginEndpoint(pool, mailgunEnv, request, response);
+    response.on('end', () => {
+      expect(response.statusCode).toEqual(200);
+      done();
+    });
+  });
 });
