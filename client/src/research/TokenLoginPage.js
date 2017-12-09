cce842d79ac837080206be262fec6e6683a7b74b
Kevin Ng <keving17@mit.edu>
2017-12-08 21:21:56 -0500

    fix linting error

--- client/src/research/TokenLoginPage.js
+++ client/src/research/TokenLoginPage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 // The page for users to login for accessing research data.
 class TokenLoginPage extends Component {
   render() {
-      return (
+    return (
       <div>
         TokenLoginPage
       </div>
