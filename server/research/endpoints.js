8c04ef177c18f6c28072240156259ab96d35d25e
Kevin Ng <keving17@mit.edu>
2018-01-18 17:06:01 -0500

    fixes json parsing in endpoints.js

--- server/research/endpoints.js
+++ server/research/endpoints.js
@@ -5,7 +5,7 @@ function interactionsEndpoint(pool, req, res) {
   pool.query(sql)
     .then(results => {
       res.set('Content-Type', 'application/json');
-      res.json(JSON.parse(JSON.stringify(results.rows)));
+      res.json({rows:results.rows});
       return res.status(200).end();
 
     })
