4780f534c43d64e1da6f5c743f7e3f75456dcce7
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-01-29 11:22:02 -0500

    Small improvements to authentication

--- server/index.js
+++ server/index.js
@@ -35,10 +35,10 @@ app.use(bodyParser.json());
 app.use(enforceHTTPS);
 const pool = createPool(config.postgresUrl);
 
-// As a precaution for emailing routes
+// As a precaution for emailing and authentication routes
 const limiter = new RateLimit({
   windowMs: 60*60*1000, // 60 minutes
-  max: 10, // limit each IP to 10 requests per windowMs
+  max: 100, // limit each IP to n requests per windowMs
   delayMs: 0, // disable delaying - full speed until the max limit is reached
   onLimitReached: (req, res, options) => {
     console.log('RateLimit reached!');
@@ -52,11 +52,11 @@ app.get('/api/peers/:workshopCode', peerResponsesEndpoint.bind(null, pool));
 app.post('/api/share', limiter, emailMyResponsesEndpoint.bind(null, config.mailgunEnv));
 
 // Endpoints for researcher login
-app.post('/api/research/login', loginEndpoint.bind(null, pool, config.mailgunEnv));
-app.post('/api/research/email', emailLinkEndpoint.bind(null, pool));
+app.post('/api/research/login', limiter, loginEndpoint.bind(null, pool, config.mailgunEnv));
+app.post('/api/research/email', limiter, emailLinkEndpoint.bind(null, pool));
 
 // Endpoints for authenticated researchers to access data
-app.get('/api/research/interactions', onlyAllowResearchers.bind(null, pool), interactionsEndpoint.bind(null, pool));
+app.get('/api/research/interactions', [limiter, onlyAllowResearchers.bind(null, pool)], interactionsEndpoint.bind(null, pool));
 
 
 // Serve any static files.
